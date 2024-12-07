996d33117400a9c4ad3fcc9445b24bad96cdc40d
town <town81495@gmail.com>
2024-12-07 22:12:18 +0900

    修復

--- even.js
+++ even.js
@@ -1,23 +1,6 @@
 'use strict';
-//fsモジュールを取得
-const fs = require("fs");
 
-////➀テキスト読み込みモジュールstart************************************************************************
-//テキストファイルを読み込む関数
-function readTextFile(filePath) {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) { console.error('エラーが発生しました:', err);
-      return;
-     }
-     const even = evenCharacters(data);
-     console.log('偶数番目の文字:', even);
-    });
-   }
-////➀テキスト読み込みモジュールend************************************************************************
-
-
-////➁偶数文字取得モジュールstart************************************************************************
-//偶数番号の文字を抽出する関数
+// 偶数番号の文字を抽出する関数
 function evenCharacters(inputString) {
 
   //結果を出力する変数resultを定義
@@ -27,12 +10,10 @@ function evenCharacters(inputString) {
   for (let i = 1; i < inputString.length; i += 2) {
       result += inputString[i];
   }
+  console.log(result);
   return result;
 }
-////➁偶数文字取得モジュールend************************************************************************
 
+//コマンドプロンプトの第一引数の文字列で実行
+evenCharacters(process.argv[2])
 
-//読み込むテキストファイルのパスを指定
-const filePath = 'C:\\Users\\town\\Desktop\\example.txt';
-//デスクトップ上のテキストファイルのパスを引数に関数を実行
-readTextFile(filePath);
