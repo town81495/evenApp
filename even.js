d7711ec5a7da088fdcdcc1682664918e834d7d65
town <town81495@gmail.com>
2024-12-07 22:33:19 +0900

    result.txtに出力する機能を実装

--- even.js
+++ even.js
@@ -4,15 +4,29 @@ const fs = require("fs");
 
 ////➀テキスト読み込みモジュールstart************************************************************************
 //テキストファイルを読み込む関数
-function readTextFile(filePath) {
+function readAndWrite(filePath) {
   fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) { console.error('エラーが発生しました:', err);
+    if (err) {console.error('エラーが発生しました:', err);
       return;
      }
+
+     //読み込んだテキストをevenCharacters関数で偶数を取得する
      const even = evenCharacters(data);
+     //ログ
      console.log('偶数番目の文字:', even);
-    });
-   }
+
+     //テキストファイルに書き込む関数
+     fs.writeFile('C:\\Users\\town\\Desktop\\result.txt', even, (err) => {
+        if (err) { console.error('ファイル書き込み中にエラーが発生しました:', err);
+        return;
+       }
+        //ログ
+        console.log(`結果が'C:\\Users\\town\\Desktop\\result.txt'に書き込まれました`);
+      });
+
+  });
+
+}
 ////➀テキスト読み込みモジュールend************************************************************************
 
 
@@ -35,4 +49,4 @@ function evenCharacters(inputString) {
 //読み込むテキストファイルのパスを指定
 const filePath = 'C:\\Users\\town\\Desktop\\example.txt';
 //デスクトップ上のテキストファイルのパスを引数に関数を実行
-readTextFile(filePath);
+readAndWrite(filePath);
